4f1da5804e3602e60b0c1ce14723299d4b9f4811
MJ <michaljalowik@gmail.com>
2020-10-22 14:24:39 +0200

    first

--- src/main/java/easysurvey/App.java
+++ src/main/java/easysurvey/App.java
@@ -3,6 +3,15 @@ package easysurvey;
 import easysurvey.persistence.SurveyService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @SpringBootApplication
 public class App {
@@ -12,6 +21,8 @@ public class App {
 		surveyService.close();
 		SpringApplication.run(App.class, args);
 	}
+
+
 }
 
 
